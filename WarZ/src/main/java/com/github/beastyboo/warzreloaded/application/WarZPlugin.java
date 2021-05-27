2359a859a4fe6570897b11ac3084480959419709
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2021-05-27 22:02:03 +0200

    Initial commit

---
+++ WarZ/src/main/java/com/github/beastyboo/warzreloaded/application/WarZPlugin.java
@@ -0,0 +1,17 @@
+package com.github.beastyboo.warzreloaded.application;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarZPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ WarZConsumable/src/main/java/com/github/beastyboo/warzfood/WarZConsumablePlugin.java
@@ -0,0 +1,17 @@
+package com.github.beastyboo.warzfood;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarZConsumablePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
